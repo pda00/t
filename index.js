b23d23e3282b98b5b33faae6d2a37a8d62e1392d
pda00 <36547803+pda00@users.noreply.github.com>
2018-10-30 15:37:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -35,3 +35,39 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
   });
 });
+
+//new
+let newWorker;
+
+  // The click event on the notification
+  document.getElementById('reload').addEventListener('click', function(){
+    newWorker.postMessage({ action: 'skipWaiting' });
+  });
+
+  if ('serviceWorker' in navigator) {
+    // Register the service worker
+    navigator.serviceWorker.register('/service-worker.js').then(reg => {
+      reg.addEventListener('updatefound', () => {
+
+        // An updated service worker has appeared in reg.installing!
+        newWorker = reg.installing;
+
+        newWorker.addEventListener('statechange', () => {
+
+          // Has service worker state changed?
+          switch (newWorker.state) {
+            case 'installed':
+
+	// There is a new service worker available, show the notification
+              if (navigator.serviceWorker.controller) {
+                let notification = document.getElementById('notification ');
+    notification .className = 'show';
+              }
+
+              break;
+          }
+        });
+      });
+    });
+
+  }
